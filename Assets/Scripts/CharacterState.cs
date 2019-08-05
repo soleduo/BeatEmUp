46e0d408067795f249c013a213c08b60e71e5d15
Rachmatilovski Valdio <r.valdio023@gmail.com>
2019-08-05 22:46:19 +0700

    CharacterState branch init

---
+++ Assets/Scripts/CharacterState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterState
+{
+    public event System.Action OnStateEnter;
+    public event System.Action OnStateEnd;
+
+    protected Character character;
+
+    public CharacterState(Character owner)
+    {
+        character = owner;
+    }
+
+    public abstract void Update();
+}
